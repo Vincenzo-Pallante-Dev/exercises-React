d6a657e2617c79d20a13e6f440d979eebf574238
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-06 13:50:25 +0200

    feat: conditional4

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Welcome } from "./components/Welcome";
+import { Hello } from "./components/Hello";
 
 export class App extends React.Component {
   render() {
     return (
       <div>
+        <Hello name={"Vincenzo"} />
         <Welcome />
       </div>
     );
