6c5e11b7f247aeaad74c2b1522a3053b294bd0ee
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-06 13:41:38 +0200

    feat: conditional3

--- my-project/src/components/Age.js
+++ my-project/src/components/Age.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export class Age extends React.Component {
   render() {
-    return <>{this.props.age && <p>La tua è {this.props.age}</p>}</>;
+    return (
+      <>
+        {this.props.age > 18 && this.props.age < 65 && (
+          <p>La tua eta è {this.props.age}</p>
+        )}
+      </>
+    );
   }
 }
