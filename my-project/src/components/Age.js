26bbd041587582ef48cbfac97bb092c3dc0057f8
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-07 18:15:24 +0200

    fix: conditional2

--- my-project/src/components/Age.js
+++ my-project/src/components/Age.js
@@ -4,7 +4,7 @@ export class Age extends React.Component {
   render() {
     return (
       <>
-        <p>La tua eta è 28 </p>
+        <p>La tua eta è {this.props.age} </p>
       </>
     );
   }
--- my-project/src/components/Welcome.js
+++ my-project/src/components/Welcome.js
@@ -3,13 +3,6 @@ import { Age } from "./Age";
 
 export class Welcome extends React.Component {
   render() {
-    return (
-      <div>
-        {/* In questo caso la props age esite */}
-        {this.props.age ? <Age /> : <p>Non esiste nessuna props.age</p>}
-        {/* In questo caso la props age "non esiste" */}
-        {!this.props.age ? <Age /> : <p>Non esiste nessuna props.age</p>}
-      </div>
-    );
+    return <div>{this.props.age && <Age age={this.props.age} />}</div>;
   }
 }
