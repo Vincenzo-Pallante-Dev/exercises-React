0020258e22ac1555eb4d30afd9e112e9ab1f6ada
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-12 18:27:56 +0200

    fix: exercise

--- my-project/src/components/ClickCounter.js
+++ my-project/src/components/ClickCounter.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { CounterButton } from "./CounterButton";
 
 export class ClickCounter extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  handleCounterIncrement = () => {
+    this.setState((state) => {
+      return {
+        count: state.count + 1,
+      };
+    });
+  };
+
   render() {
-    return <CounterButton />;
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleCounterIncrement}>Incrementa</button>
+      </div>
+    );
   }
 }
