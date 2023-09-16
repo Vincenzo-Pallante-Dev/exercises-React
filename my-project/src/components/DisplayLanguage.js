87369942f073fb0f53b509c05de2f0514f282656
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-16 03:13:54 +0200

    feat: exercise

--- my-project/src/components/DisplayLanguage.js
+++ my-project/src/components/DisplayLanguage.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { LanguageContext } from "./LanguageContext";
 
 export class DisplayLanguage extends React.Component {
   render() {
-    return <></>;
+    return (
+      <div>
+        <LanguageContext.Consumer>
+          {(language) => {
+            return <h1>{language}</h1>;
+          }}
+        </LanguageContext.Consumer>
+      </div>
+    );
   }
 }
