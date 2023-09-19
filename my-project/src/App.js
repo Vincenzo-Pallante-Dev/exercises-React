1c14b98f86b93dbd423819cbf82ac7515639cb31
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-19 19:35:09 +0200

    feat: exercise

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { LoginForm } from "./components/Login";
+import { ClickCounter } from "./components/ClickCounter";
 
 export class App extends React.Component {
   render() {
-    return <LoginForm />;
+    return <ClickCounter />;
   }
 }
