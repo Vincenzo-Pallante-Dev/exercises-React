92e103c3f5e20b6c4f73ffe109a0acf11b458b48
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-19 17:52:51 +0200

    feat: exercise

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,32 +1,8 @@
 import React from "react";
-import { LanguageContext } from "./components/LanguageContext";
-import { DisplayLanguage } from "./components/DisplayLanguage";
+import { ClickCounter } from "./components/ClickCounter";
 
 export class App extends React.Component {
-  state = {
-    language: "it",
-  };
-
-  handleLanguageChange = (event) => {
-    this.setState({
-      language: event.target.value,
-    });
-  };
-
   render() {
-    return (
-      <div>
-        <select
-          value={this.state.language}
-          onChange={this.handleLanguageChange}
-        >
-          <option value="it">Italiano</option>
-          <option value="en">Inglese</option>
-        </select>
-        <LanguageContext.Provider value={this.state.language}>
-          <DisplayLanguage />
-        </LanguageContext.Provider>
-      </div>
-    );
+    return <ClickCounter />;
   }
 }
