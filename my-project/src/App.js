faeed1cc5e34c745ec19476d543b20ed475a19bf
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-26 19:25:32 +0200

    feat: exercise

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,5 +1,12 @@
-import { Counter } from "./components/Counter";
+import { FilteredList } from "./FilteredList";
+
+const items = [
+  { id: 1, nome: "Vincenzo", età: "28" },
+  { id: 2, nome: "Antonio", età: "10" },
+  { id: 3, nome: "Alessandro", età: "28" },
+  { id: 3, nome: "Gianmarco", età: "17" },
+];
 
 export function App() {
-  return <Counter />;
+  return <FilteredList items={items} />;
 }
