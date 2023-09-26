747bf9b509107d96bf43237ee4e52ddf8930cd61
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-26 19:06:25 +0200

    feat: exercise

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,23 +1,5 @@
-import { useState } from "react";
-import { DisplayLanguage } from "./components/DisplayLanguage";
-import { LanguageContext } from "./components/LanguageContext";
+import { Counter } from "./components/Counter";
 
 export function App() {
-  const [language, setLanguage] = useState("it");
-
-  function handleChangeLinguage(event) {
-    setLanguage(event.target.value);
-  }
-
-  return (
-    <div>
-      <select value={language} onChange={handleChangeLinguage}>
-        <option value="it">Italiano</option>
-        <option value="en">English</option>
-      </select>
-      <LanguageContext.Provider value={language}>
-        <DisplayLanguage />
-      </LanguageContext.Provider>
-    </div>
-  );
+  return <Counter />;
 }
