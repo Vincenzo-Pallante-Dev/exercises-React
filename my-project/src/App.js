135ae61c6df5ed71fa5ff270a901c2c38d5f33e5
Vincenzo Pallante <vincenzopallante0@gmail.com>
2023-09-29 17:59:25 +0200

    feat: exercise

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Links } from "./components/Links";
 
 export function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Links />} />
+        <Route path="links" element={<Links />} />
+        <Route path="*" element={<div>
+          <p>Not found</p>
+          <Link to="links">Go page link</Link>
+        </div>}/>
       </Routes>
     </div>
   );
