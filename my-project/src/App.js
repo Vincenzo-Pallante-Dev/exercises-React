7826e203b7532fde8b60d4d670da4a354ed140a2
Vincenzo Pallante <vincenzopallante0@gmail.com>
2023-09-30 18:32:21 +0200

    feat: exercise

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import { ShowGithubUser } from "./components/ShowGithubUser";
-
+import { Routes, Route } from "react-router-dom";
+import { GithubUser } from "./components/GithubUser";
 
 export function App() {
   return (
     <div>
-      <Link to="/users">Go ShowGithubUser Component</Link>
-      <hr/>
-      <Link to="/">Go Home</Link>
       <Routes>
-        <Route path="/users" element={<ShowGithubUser />} >
-          <Route index element={<p>Aggiungi un utente e selezionalo</p>} />
-        </Route>
+        <Route path="/" element={<GithubUser />} />
       </Routes>
     </div>
   );
 }
-
