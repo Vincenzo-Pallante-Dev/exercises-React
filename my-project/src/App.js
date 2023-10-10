a6d8a60b1e14fab6a0c47109cf57ac599c0e2972
Vincenzo <vincenzopallante@icoud.com>
2023-10-10 21:28:00 +0200

    fix: exercise

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
 import { ShowGithubUser } from "./components/ShowGithubUser";
 
 export function App() {
   return (
     <div>
       <Link to="/users">Go ShowGithubUser Component</Link>
-      <hr/>
+      <hr />
       <Link to="/">Go Home</Link>
       <Routes>
-        <Route path="/users" element={<ShowGithubUser />} />
+        <Route path="/users" element={<ShowGithubUser />}>
+          <Route index element={<Outlet />} />
+        </Route>
       </Routes>
     </div>
   );
 }
-
