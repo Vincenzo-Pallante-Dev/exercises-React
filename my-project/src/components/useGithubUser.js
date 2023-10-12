a467057f9adb9e94d6ba7e3f9637d691f0178677
Vincenzo <vincenzopallante@icoud.com>
2023-10-12 18:24:48 +0200

    fix: exercise

--- my-project/src/components/useGithubUser.js
+++ my-project/src/components/useGithubUser.js
@@ -1,7 +1,9 @@
 import useSWR from "swr";
 
 export function useGithubUser(username) {
-  const { data, error } = useSWR(`https://api.github.com/users/${username}`);
+  const { data, error } = useSWR(
+    data !== null && `https://api.github.com/users/${username}`
+  );
 
   return {
     data,
